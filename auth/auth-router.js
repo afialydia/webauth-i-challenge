516d9be8e3a2c579990f5f26cf263b8edb015faa
Afia-Lydia <you@example.com>
2019-12-17 17:33:52 -0500

    met mvp

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,6 @@
-const bcrypt = require("bcryptjs");
 
 const router = require("express").Router();
-
+const bcrypt = require("bcryptjs");
 const Users = require("../users/user-model.js");
 
 router.post("/register", (req, res) => {
@@ -25,15 +24,16 @@ router.post("/register", (req, res) => {
 router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
-  // check that the password
-
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        // in here with .compare()
-        // change the users-model findBy() to return the password as well
-        res.status(200).json({ message: `Welcome ${user.username}!` });
+        // save a session for the client and send back a cookie
+        req.session.user = user;
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+        });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
       }
@@ -41,6 +41,24 @@ router.post("/login", (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(error => {
+      if (error) {
+        res.status(500).json({
+          message:
+            "you can checkout any time you like, but you can never leave!!!!!",
+        });
+      } else {
+        res.status(200).json({ message: "logged out" });
+      }
+    });
+  } else {
+    res.status(200).end();
+  }
 });
 
 module.exports = router;
--- users/user-router.js
+++ users/user-router.js
@@ -1,15 +1,14 @@
 const router = require("express").Router();
-
 const Users = require("./user-model.js");
+const restricted = require("../auth/restricted-middleware.js");
 
-router.get("/", (req, res) => {
+router.get("/", restricted, (req, res) => {
 	Users.find()
 		.then(users => {
-
 			res.json(users);
 		})
 		.catch(err => {
-			res.status(500).json({ message: "Failed to get list of projects" });
+			res.status(500).json({ message: "Failed to get list of users" });
 		});
 });
 
