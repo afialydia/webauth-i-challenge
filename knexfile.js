d87dfa8a8e95345729f150ed8854b6870ee2dae7
Afia-Lydia <you@example.com>
2019-12-17 15:39:44 -0500

    met mvp for day 1. created log in and authorization for users and endpoints to use them

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/users.db3'
     },
     pool: {
       afterCreate: (conn, done) => {
@@ -13,10 +13,10 @@ module.exports = {
       },
     },
     migrations: {
-      directory: "./database/migrations",
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: "./database/seeds",
+      directory: "./data/seeds",
     },
   },
 };
